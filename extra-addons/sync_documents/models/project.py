0c71958467b2ab7fca00093b8e95d1b4ec911687
KasNG <kasngvn@gmail.com>
2020-08-14 15:48:03 +0700

    Updated project document

--- extra-addons/sync_documents/models/project.py
+++ extra-addons/sync_documents/models/project.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models, tools, SUPERUSER_ID, _
 import ast
 
+
 class DHAProject(models.Model):
     _inherit = "project.project"
 
@@ -14,3 +15,17 @@ class DHAProject(models.Model):
             domain.append(('id', 'in', dha_documents.attachment_id.ids))
             action['domain'] = str(domain)
         return action
+
+    def _compute_attached_docs_count(self):
+        Attachment = self.env['ir.attachment']
+        dha_documents = self.env['dha.document'].search([('project_id', '=', self.id)])
+        for project in self:
+            project.doc_count = Attachment.search_count([
+                '|',
+                '|',
+                '&',
+                ('res_model', '=', 'project.project'), ('res_id', '=', project.id),
+                '&',
+                ('res_model', '=', 'project.task'), ('res_id', 'in', project.task_ids.ids),
+                ('id', 'in', dha_documents.attachment_id.ids)
+            ])

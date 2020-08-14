13b7176a214f0806f15dbd79beb5de91504447e5
KasNG <kasngvn@gmail.com>
2020-08-14 17:56:42 +0700

    Updated project document

--- extra-addons/sync_documents/models/project.py
+++ extra-addons/sync_documents/models/project.py
@@ -8,7 +8,7 @@ class DHAProject(models.Model):
     sync_doc_count = fields.Integer(compute='_compute_syned_docs_count', string="Number of sync documents attached")
 
     def sync_attachment_tree_view(self):
-        attachment_action = self.env.ref('base.action_attachment')
+        attachment_action = self.env.ref('sync_documents.action_document_dashboard')
         action = attachment_action.read()[0]
         # Get data from dha_document
         dha_documents = self.env['dha.document'].search([('project_id', '=', self.id)])
@@ -16,18 +16,20 @@ class DHAProject(models.Model):
             ('id', 'in', dha_documents.attachment_id.ids),
         ])
         action['context'] = "{'default_res_model': '%s','default_res_id': %d}" % (self._name, self.id)
+        action['display_name'] = _('Operation Documents')
         return action
 
-    # def attachment_tree_view(self):
-    #     action = super(DHAProject, self).attachment_tree_view()
-    #     domain = ast.literal_eval(action['domain'])
-    #     # Get data from dha_document
-    #     dha_documents = self.env['dha.document'].search([('project_id', '=', self.id)])
-    #     if dha_documents:
-    #         domain.insert(0, '|')
-    #         domain.append(('id', 'in', dha_documents.attachment_id.ids))
-    #         action['domain'] = str(domain)
-    #     return action
+    def attachment_tree_view(self):
+        action = super(DHAProject, self).attachment_tree_view()
+        action['display_name'] = _('Documents')
+        # domain = ast.literal_eval(action['domain'])
+        # # Get data from dha_document
+        # dha_documents = self.env['dha.document'].search([('project_id', '=', self.id)])
+        # if dha_documents:
+        #     domain.insert(0, '|')
+        #     domain.append(('id', 'in', dha_documents.attachment_id.ids))
+        #     action['domain'] = str(domain)
+        return action
 
     def _compute_syned_docs_count(self):
         Attachment = self.env['ir.attachment']
